04045f98eeeff52f5c6a9fa908f32875c3f03ba8
arifu00 <arifu5758@gmail.com>
2023-08-21 13:29:04 +0600

    discount part manipolations

--- script/cardTitle.js
+++ script/cardTitle.js
@@ -4,6 +4,8 @@ let price = 0;
 function cardClick(target) {
     const selectedItem = document.getElementById('show-item-name');
     const totalPriceId = document.getElementById('total-price');
+    const grandTotalId = document.getElementById('grand-total');
+
     const itemName = target.childNodes[3].childNodes[5].innerText;
     const li = document.createElement('li');
     li.innerText = itemName;
@@ -13,23 +15,47 @@ function cardClick(target) {
     price = parseFloat(price) + parseFloat(itemPrice);
     itemTotalPrice = price.toFixed(2)
     totalPriceId.innerText = itemTotalPrice;
-    
+    grandTotalId.innerText = itemTotalPrice;
+
 
     // Purchase btn enable 
-     const purchaseBtn = document.getElementById('purchase-btn');
-     if(itemTotalPrice > 0){
+    const purchaseBtn = document.getElementById('purchase-btn');
+    if (itemTotalPrice > 0) {
         purchaseBtn.removeAttribute('disabled')
-     }
+    }
 
     //  apply btn enable 
     const applyBtn = document.getElementById('apply-btn');
-    if(itemTotalPrice > 200){
+    if (itemTotalPrice > 200) {
         applyBtn.removeAttribute('disabled');
     }
 
+}
+document.getElementById("apply-btn").addEventListener("click", function () {
+    const couponCodeInput = document.getElementById('coupon-code');
+    const couponCode = couponCodeInput.value;
+
+    const grandTotalId = document.getElementById('grand-total');
+    const discount = document.getElementById('discount');
+    const totalPrice = itemTotalPrice;
+
+    couponCodeInput.value = '';
     
 
-    };
+    if (couponCode === "SELL200") {
+        discountPercentage = 20 / 100;
+        discountPrice =  (totalPrice * discountPercentage).toFixed(2);
+        discount.innerText = discountPrice;
+        grandTotalId.innerText = (totalPrice - (totalPrice * discountPercentage)).toFixed(2);
+        
+
+        
+    } else {
+        
+        alert("Coupon code is not valid.");
+    }
+   
+});
 
 
 
